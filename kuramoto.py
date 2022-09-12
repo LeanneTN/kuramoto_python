0df684474669b78eee611f3c94477795a483e03c
LeanneTN <2321604632@qq.com>
2022-09-12 23:33:00 +0800

    docs: readme intro

--- kuramoto.py
+++ kuramoto.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint
 
+
 class Kuramoto:
 
     def __init__(self, dt, coupling, T, n_nodes, natfreqs=None):
@@ -16,14 +17,14 @@ class Kuramoto:
             self.nafreqs = np.random.normal(size=self.n_nodes)
 
     def init_angles(self):
-        '''
+        """
         randomly initialize angles (position, theta)
         theta is initial position of one oscillator
-        '''
+        """
         return 2 * np.pi * np.random.random(size=self.n_nodes)
 
     def derivative(self, angle_vectors, t, adj_mat, coupling):
-        '''
+        """
         derivative function. Original equation can watch the pictures
         under image directory in same name
         :param angle_vectors:
@@ -31,7 +32,7 @@ class Kuramoto:
         :param adj_mat: A_ij in original equation, means adjacent matrix
         :param coupling: k/M in original equation
         :return: matrix
-        '''
+        """
 
         assert len(angle_vectors) == len(self.nafreqs) == len(adj_mat), \
             'input dimensions do not match, check lengths'
@@ -47,32 +48,32 @@ class Kuramoto:
         return dx_dt
 
     def integrate(self, angle_vectors, adj_matrix):
-        '''
+        """
         updates all states by integrating all nodes' states in matrix
         :param angle_vectors: vectors of each angle
         :param adj_matrix: adjacent matrix
         :return:
-        '''
+        """
         # number of incoming interactions
         n_interactions = (adj_matrix != 0).sum(axis=0)
         # normalize coupling by number of interactions
         coupling = self.coupling / n_interactions
 
-        t = np.linspace(0, self.T, int(self.T/self.dt))
+        t = np.linspace(0, self.T, int(self.T / self.dt))
         # derivative是微分方程, angle_vectors是微分方程的初值, t是微分的自变量
         # odeint是一个用于求解微分方程的函数
         time_series = odeint(self.derivative, angle_vectors, t, args=(adj_matrix, coupling))
         return time_series.T
 
     def run(self, adj_matrix=None, angle_vectors=None):
-        '''
+        """
         this function is for run the script
         :param adj_matrix: adjacent matrix, nd array
         2D: show the connectivity of each node
         :param angle_vectors: angle vectors
         1D: states vectors of nodes shows the position in radians
         :return: integrated matrix, 2D nd array
-        '''
+        """
         if angle_vectors is None:
             angle_vectors = self.init_angles()
 
@@ -80,25 +81,25 @@ class Kuramoto:
 
     @staticmethod
     def phase_coherence(angles_vectors):
-        '''
+        """
         this function is for compute global order parameter, namely Rt in
         original equation
         the original equation of Rt is under the image directory and named
         with order_parameter
         :param angles_vectors: np array
         :return: np array
-        '''
+        """
         # 1j为虚数单位
         temp_sum = sum([(np.e ** (1j * i)) for i in angles_vectors])
         return abs(temp_sum / len(angles_vectors))
 
     def mean_frequency(self, act_matrix, adj_matrix):
-        '''
+        """
         average frequency within the time window (namely self.T) for all nodes
         :param act_matrix: np array, the integrated adjacent matrix
         :param adj_matrix: adjacent matrix, np array
         :return: np array 2D/1D
-        '''
+        """
         assert len(adj_matrix) == act_matrix.shape[0], 'adjacent matrix does not match act matrix'
         _, n_steps = act_matrix.shape
 
@@ -112,4 +113,3 @@ class Kuramoto:
         # average across complete time window - mean angular velocity
         mean_freq = integrates / self.T
         return mean_freq
-
